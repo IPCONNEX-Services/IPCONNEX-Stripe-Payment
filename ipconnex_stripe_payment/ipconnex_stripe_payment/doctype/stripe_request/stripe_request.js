d512e941b084d3af0e5ab58c22df35650eaabdbf
A-RAMZI <73026384+A-RAMZI@users.noreply.github.com>
2024-04-05 01:10:29 +0100

    commit

--- ipconnex_stripe_payment/ipconnex_stripe_payment/doctype/stripe_request/stripe_request.js
+++ ipconnex_stripe_payment/ipconnex_stripe_payment/doctype/stripe_request/stripe_request.js
@@ -2,5 +2,20 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Stripe Request', {
-
+    refresh:function(frm){
+        $("button[data-fieldname='generate_key']").off("click").on("click",(event)=>{
+             console.log("Generate Key")})
+    },
+    sales_invoice:function(frm){
+             console.log("sales_invoice changed")
+    },
+    sales_order:function(frm){
+             console.log("sales_order changed")
+    },
+    customer:function(frm){
+        console.log("customer changed")
+    },
+    is_supplier:function(frm){
+        console.log("is supplier changed")
+    },
 });
