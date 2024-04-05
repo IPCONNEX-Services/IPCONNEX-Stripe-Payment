2226c6f5879aed32be5c17033c8e9c01feb9e1b8
A-RAMZI <73026384+A-RAMZI@users.noreply.github.com>
2024-04-05 04:58:11 +0100

    commit

--- ipconnex_stripe_payment/ipconnex_stripe_payment/doctype/stripe_request/stripe_request.js
+++ ipconnex_stripe_payment/ipconnex_stripe_payment/doctype/stripe_request/stripe_request.js
@@ -13,9 +13,67 @@ frappe.ui.form.on('Stripe Request', {
              console.log("sales_order changed")
     },
     customer:function(frm){
-        console.log("customer changed")
+        if(frm.doc.customer && cur_frm.doc.request_type=="Customer" ){
+	        frappe.call({
+                    method: "frappe.desk.form.load.getdoc",
+                    args:{
+                        doctype:"Customer",
+                        name:frm.doc.customer
+                    },
+                    callback: function(res_customer) {
+                        var dettes=0.00;
+                        frappe.call({
+                            method: "frappe.desk.form.load.getdoc",
+                            args:{
+                                doctype:"Supplier",
+                                name:frm.doc.customer
+                            },
+                            callback: function(res_supplier) {
+                                    if(res_supplier["docs"]  && frm.doc.is_supplier ){
+                                        dettes=(Math.round(res_customer["docs"][0]["__onload"]["dashboard_info"][0]["total_unpaid"]*100)-Math.round(res_supplier["docs"][0]["__onload"]["dashboard_info"][0]["total_unpaid"]*100) )/100 ;
+                                    }else{
+                                        try{
+                                            dettes=Math.round(res_customer["docs"][0]["__onload"]["dashboard_info"][0]["total_unpaid"]*100)/100 ;
+                                        }catch(error){
+                                        }
+                                    }
+                                    frm.set_value({"requested_amount":dettes});
+                                    }
+                            });
+                    }
+            });
+	    }
     },
     is_supplier:function(frm){
-        console.log("is supplier changed")
+        if(frm.doc.customer  && cur_frm.doc.request_type=="Customer" ){
+	        frappe.call({
+                    method: "frappe.desk.form.load.getdoc",
+                    args:{
+                        doctype:"Customer",
+                        name:frm.doc.custome
+                    },
+                    callback: function(res_customer) {
+                        var dettes=0.00;
+                        frappe.call({
+                            method: "frappe.desk.form.load.getdoc",
+                            args:{
+                                doctype:"Supplier",
+                                name:frm.doc.customer
+                            },
+                            callback: function(res_supplier) {
+                                    if(res_supplier["docs"] && frm.doc.is_supplier ){
+                                        dettes=(Math.round(res_customer["docs"][0]["__onload"]["dashboard_info"][0]["total_unpaid"]*100)-Math.round(res_supplier["docs"][0]["__onload"]["dashboard_info"][0]["total_unpaid"]*100) )/100 ;
+                                    }else{
+                                        try{
+                                            dettes=Math.round(res_customer["docs"][0]["__onload"]["dashboard_info"][0]["total_unpaid"]*100)/100 ;
+                                        }catch(error){
+                                        }
+                                    }
+                                    frm.set_value({"requested_amount":dettes});
+                                    }
+                            });
+                    }
+            });
+	    }
     },
 });
