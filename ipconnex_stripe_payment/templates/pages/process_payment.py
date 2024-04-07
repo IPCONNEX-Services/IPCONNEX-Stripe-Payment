103791a808b058340ad521b57de09909f679fa66
A-RAMZI <73026384+A-RAMZI@users.noreply.github.com>
2024-04-08 00:23:42 +0100

    commit

--- ipconnex_stripe_payment/templates/pages/process_payment.py
+++ ipconnex_stripe_payment/templates/pages/process_payment.py
@@ -1,3 +1,7 @@
 
 def get_context(context):
-    context.custom_context_variable = "Hello, World!"
+    context.custom_context_variable = "Hello, World!"    
+    # Accessing the URL parameter "param"
+    param_value = frappe.form_dict.get('param', 'default_value')
+    # You can then add this to the context passed to the Jinja template
+    context.param_value = param_value
