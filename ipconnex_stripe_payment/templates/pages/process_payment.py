eeb6548e899a73ab33d099742ef7d12338e6c309
A-RAMZI <73026384+A-RAMZI@users.noreply.github.com>
2024-04-14 00:47:26 +0100

    commit

--- ipconnex_stripe_payment/templates/pages/process_payment.py
+++ ipconnex_stripe_payment/templates/pages/process_payment.py
@@ -2,8 +2,21 @@ import frappe
 
 
 def get_context(context):
-    context.custom_context_variable = "Hello, World!"    
-    # Accessing the URL parameter "param"
-    param_value = frappe.form_dict.get('param', 'default_value')
-    # You can then add this to the context passed to the Jinja template
-    context.param_value = param_value
+    payment_token  = frappe.form_dict.get('token', '')
+    context.payment_token = payment_token 
+    stripe_settings=frappe.db.get_all("Stripe Settings",
+            fields=["publishable_key"],order_by='modified', limit_page_length=0)
+    if(len(stripe_settings)!=0):
+        context.public_stripe =stripe_settings[0].publishable_key
+
+    stripe_requests=frappe.db.get_all("Stripe Request",
+                    filters={"payment_url": "/process_payment?token="+payment_token },
+                    fields=["request_type","customer","sales_invoice","sales_order","requested_amount","currency"],order_by='modified', limit_page_length=0)
+    
+    if(len(stripe_requests)==0):
+        context.request_type=stripe_requests[0].request_type
+        context.customer=stripe_requests[0].customer
+        context.sales_invoice=stripe_requests[0].sales_invoice
+        context.sales_order=stripe_requests[0].sales_order
+        context.requested_amount=stripe_requests[0].requested_amount
+        context.currency=stripe_requests[0].currency
