0882e3a1ede1c22af24c8e7ea82d2e601757ce76
A-RAMZI <73026384+A-RAMZI@users.noreply.github.com>
2024-04-14 16:57:36 +0100

    commit

--- ipconnex_stripe_payment/templates/pages/process_payment.py
+++ ipconnex_stripe_payment/templates/pages/process_payment.py
@@ -1,14 +1,16 @@
 import frappe
 
 
+no_cache = 1
+
 def get_context(context):
-    payment_token  = frappe.form_dict["token"]
-    context.payment_token = payment_token 
+    context.no_cache = 1
     stripe_settings=frappe.db.get_all("Stripe Settings",
             fields=["publishable_key"],order_by='modified', limit_page_length=0)
     if(len(stripe_settings)!=0):
         context.public_stripe =stripe_settings[0].publishable_key
-
+    payment_token  = frappe.form_dict["token"]
+    context.payment_token = payment_token 
     stripe_requests=frappe.db.get_all("Stripe Request",
                     filters={"payment_url": "/process_payment?token="+payment_token },
                     fields=["request_type","customer","sales_invoice","sales_order","requested_amount","currency"],order_by='modified', limit_page_length=0)
@@ -19,11 +21,5 @@ def get_context(context):
         context.sales_order=stripe_requests[0].sales_order
         context.requested_amount=stripe_requests[0].requested_amount
         context.currency=stripe_requests[0].currency
-
-
-
-
-
-
 
 
