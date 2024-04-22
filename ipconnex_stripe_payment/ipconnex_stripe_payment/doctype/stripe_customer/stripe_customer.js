60aef499852149ba617bb14c5a413138f32aa1a4
A-RAMZI <73026384+A-RAMZI@users.noreply.github.com>
2024-04-22 21:44:39 +0100

    commit

--- ipconnex_stripe_payment/ipconnex_stripe_payment/doctype/stripe_customer/stripe_customer.js
+++ ipconnex_stripe_payment/ipconnex_stripe_payment/doctype/stripe_customer/stripe_customer.js
@@ -1,6 +1,36 @@
-// Copyright (c) 2020, Frappe and contributors
-// For license information, please see license.txt
+var script = document.createElement('script');
+script.src = 'https://cdn.jsdelivr.net/npm/sweetalert2@10';
+document.head.appendChild(script);
 
 frappe.ui.form.on('Stripe Customer', {
+    refresh:function(frm){
 
+        $("button[data-fieldname='get_stripe_id']").off("click").on("click",
+            function(){  
+                frappe.call({
+                    method: "ipconnex_stripe_payment.ipconnex_stripe_payment.payement.getCustomer",
+                    args: {email:frm.doc.email, full_name:frm.doc.customer,sec_key:"sk_test_51CJ0txGmGCEPYxBaRPZlAeZMOMQySVQANE6E8dgrXsK075nlGu3G1amWhnkAKoG04HoP7qihiqOieGPkyosvj0BN00hRDOTR42"
+                    },
+                    callback: function(res){ 
+                        console.log(res.status);
+                        if(res.status==1){ 
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'Success',
+                                text: res.message,
+                            });
+                        }else{ 
+                            Swal.fire({
+                            icon: "warning",
+                            title: "Warning",
+                            text: res.message,
+                        });
+
+                        }
+                    
+                    
+                    
+                    }});
+        } );
+    }
 });
