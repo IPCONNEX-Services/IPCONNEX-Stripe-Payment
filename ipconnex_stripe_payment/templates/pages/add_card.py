35fca6baa965b0e9c27ef88f310f1761ddd9ce1f
A-RAMZI <73026384+A-RAMZI@users.noreply.github.com>
2024-04-24 23:24:51 +0100

    commit

--- ipconnex_stripe_payment/templates/pages/add_card.py
+++ ipconnex_stripe_payment/templates/pages/add_card.py
@@ -10,17 +10,14 @@ def get_context(context):
             fields=["publishable_key"],order_by='modified', limit_page_length=0)
     if(len(stripe_settings)!=0):
         context.public_stripe =stripe_settings[0].publishable_key
-    payment_token  = frappe.form_dict["token"]
-    context.payment_token = payment_token 
-    stripe_requests=frappe.db.get_all("Stripe Request",
-                    filters={"payment_url": "/process_payment?token="+payment_token },
-                    fields=["request_type","customer","sales_invoice","sales_order","requested_amount","currency"],order_by='modified', limit_page_length=0)
-    if(len(stripe_requests)!=0):
-        context.request_type=stripe_requests[0].request_type
-        context.customer=stripe_requests[0].customer
-        context.sales_invoice=stripe_requests[0].sales_invoice
-        context.sales_order=stripe_requests[0].sales_order
-        context.requested_amount=stripe_requests[0].requested_amount
-        context.currency=stripe_requests[0].currency
+    client_token = frappe.form_dict["token"]
+    context.client_token = client_token
+    stripe_customers=frappe.db.get_all("Stripe Customer",
+                    filters={"payment_url":client_token },
+                    fields=["name","email","card_token"],order_by='modified', limit_page_length=0)
+    if(len(stripe_customers)!=0):
+        context.customer=stripe_customers[0].name
+        context.email=stripe_customers[0].email
+        context.card_token=stripe_customers[0].card_token
     context.no_cache = 1
 
