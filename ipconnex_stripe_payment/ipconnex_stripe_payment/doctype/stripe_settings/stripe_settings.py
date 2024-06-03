a368b8e901f6215ce13b8841af5674f96ea08cac
A-RAMZI <73026384+A-RAMZI@users.noreply.github.com>
2024-06-03 06:05:51 +0100

    commit

--- ipconnex_stripe_payment/ipconnex_stripe_payment/doctype/stripe_settings/stripe_settings.py
+++ ipconnex_stripe_payment/ipconnex_stripe_payment/doctype/stripe_settings/stripe_settings.py
@@ -7,9 +7,23 @@ from frappe.model.document import Document
 
 class StripeSettings(Document):
     def get(self, *args, **kwargs):
-        if frappe.session.user == "Administrator" or frappe.local.request_ip.startswith("127.0.0"):
+        user_roles = frappe.get_all("Has Role", filters={"parent": frappe.session.user}, fields=["role"])
+        user_roles = [role.role for role in user_roles]
+        is_admin= "System Manager" in user_roles or "Accounts Manager" in user_roles
+        if is_admin or frappe.local.request_ip.startswith("127.0.0"):
             pass
         else: 	
             self.secret_key = "*"*len(self.secret_key)
         return super().get(*args, **kwargs)
-	
+    
+
+    def before_save(self):
+        user_roles = frappe.get_all("Has Role", filters={"parent": frappe.session.user}, fields=["role"])
+        user_roles = [role.role for role in user_roles]
+        is_admin = "System Manager" in user_roles or "Accounts Manager" in user_roles
+        if not is_admin:
+            frappe.throw("The current user does not have the required admin role to edit Stripe Settings.")
+
+            
+    def validate(self):
+        self.get()
