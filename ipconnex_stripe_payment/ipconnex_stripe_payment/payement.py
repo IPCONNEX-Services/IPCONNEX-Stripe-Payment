1e908ffaad6b4ae788280bffb009f1f577b7765c
A-RAMZI <73026384+A-RAMZI@users.noreply.github.com>
2024-09-21 14:21:38 +0100

    commit

--- ipconnex_stripe_payment/ipconnex_stripe_payment/payement.py
+++ ipconnex_stripe_payment/ipconnex_stripe_payment/payement.py
@@ -7,6 +7,7 @@ from frappe import _
 from frappe.utils import flt
 import stripe
 import random
+from datetime import datetime
 
 def setup_install():
     try:
@@ -537,7 +538,7 @@ def process_subscription(user_sub,sub_type):
     posting_date= frappe.utils.nowdate()
     due_date= frappe.utils.add_days(posting_date, +30)
     rate=20
-    last_sub_day=user_sub_doc.last_sub_day
+    last_sub_day=datetime.strptime(user_sub_doc.last_sub_day, "%Y-%m-%d").date() 
     item_prices_list=frappe.get_all("Item Price",fields=["price_list_rate"],filters={"item_code":"Microsoft 365 Business Premium","selling":1})
     if(len(item_prices_list)!=0):
         rate=item_prices_list[0]["price_list_rate"]
@@ -625,7 +626,7 @@ def process_subscription(user_sub,sub_type):
 
             from_date=frappe.utils.nowdate() 
             if(last_sub_day ):
-                if(frappe.utils.nowdate() <= last_sub_day):
+                if(frappe.utils.nowdate() <= last_sub_day): 
                     from_date= frappe.utils.add_days(last_sub_day, 1)
             to_date=frappe.utils.add_days(from_date, sub_type_doc.duration)
             subscription_list=[{
@@ -642,9 +643,13 @@ def process_subscription(user_sub,sub_type):
             user_sub_doc.last_sub_day=to_date 
             user_sub_doc.save(ignore_permissions=True)  
             return result
+        
+        except stripe.error.StripeError as e:
+            payment_method_id=""
+            result= {"message":"Echec"+str(e),"status":0}
         except Exception as e: 
             payment_method_id=""
             result= {"message":"Echec"+str(e),"status":0}
             return result
-    #frappe.delete_doc("Sales Invoice", invoice_doc.name)
+    frappe.delete_doc("Sales Invoice", invoice_doc.name)
     return result
