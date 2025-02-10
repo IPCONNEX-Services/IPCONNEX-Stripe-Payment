db016a3a0d9e429de8629599feaa105ebb2d3c2b
A-RAMZI <73026384+A-RAMZI@users.noreply.github.com>
2025-02-10 17:39:04 +0100

    commit

--- ipconnex_stripe_payment/ipconnex_stripe_payment/payement.py
+++ ipconnex_stripe_payment/ipconnex_stripe_payment/payement.py
@@ -743,7 +743,7 @@ def process_subscription(user_sub,sub_type):
             mail_content = f"""<h3>Subscription </h3>
                 <p> Hello {user_sub_doc.stripe_customer},</p>
                 <p> Your subscription on our portal has been approved ! </p>  
-                <p> Payed Amount: **** {to_pay} </p> 
+                <p> Payed Amount:  {to_pay:.2f} $ </p> 
                 <p> Paiment Reference : **** {stripe_card.last_digits}/{payment_intent.id} </p> 
                 <p> Thank you !<br> </p> """
                
